413791fcd08cff5e4455f8cd8734e0b2385acd8c
mutiulahi <tesleemolamilekan902@gmail.com>
2023-01-06 14:33:52 +0100

    hi

--- includes/database.php
+++ includes/database.php
@@ -1,14 +1,31 @@
 <?php
 // database connection
 
-$dbconnect = mysqli_connect("dbaas-db-5831699-do-user-12325480-0.b.db.ondigitalocean.com", "doadmin", "AVNS_6yeUKio300fObf6VPBg", "defaultdb");
-if(!$dbconnect){
-    die("Connection failed: " . mysqli_connect_error());
-}else{
-    echo "Connected successfully";
-}
-
-// $dbconnect = mysqli_connect("localhost", "root", "", "rashak_db");
+// $dbconnect = mysqli_connect("dbaas-db-5831699-do-user-12325480-0.b.db.ondigitalocean.com", "doadmin", "AVNS_6yeUKio300fObf6VPBg", "defaultdb");
 // if(!$dbconnect){
 //     die("Connection failed: " . mysqli_connect_error());
+// }else{
+//     echo "Connected successfully";
 // }
+
+// // $dbconnect = mysqli_connect("localhost", "root", "", "rashak_db");
+// // if(!$dbconnect){
+// //     die("Connection failed: " . mysqli_connect_error());
+// // }
+
+
+$host = 'http://157.245.132.117/'; //188.166.....
+$user = 'tescode';
+$pass = 'tescode';
+$db = 'rashak_db';
+
+$conn=mysqli_connect($host,$user,$pass,$db);
+
+if($conn)
+{
+	echo "OK";
+}
+else
+{
+	echo "Error";
+}

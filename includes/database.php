46ee6656172845e97cbc90380c5d2bed35484ffe
mutiulahi <tesleemolamilekan902@gmail.com>
2023-01-06 14:26:43 +0100

    hi

--- includes/database.php
+++ includes/database.php
@@ -1,7 +1,7 @@
 <?php
 // database connection
 
-$dbconnect = mysqli_connect("157.245.132.117", "root", "tescode", "rashak_db");
+$dbconnect = mysqli_connect("dbaas-db-5831699-do-user-12325480-0.b.db.ondigitalocean.com", "doadmin", "AVNS_6yeUKio300fObf6VPBg", "defaultdb");
 if(!$dbconnect){
     die("Connection failed: " . mysqli_connect_error());
 }else{

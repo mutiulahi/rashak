69618f72b60d0e50aa22a7ac214b0a6c1d8baa5a
mutiulahi <tesleemolamilekan902@gmail.com>
2023-01-07 16:09:10 +0100

    remove search bar

--- layout/header.php
+++ layout/header.php
@@ -41,6 +41,16 @@
                     <button class="navbar-toggler p-0 border-0 menu-toggle order-3" type="button" data-bs-toggle="collapse" data-bs-target="#mainHeader">
                         <span class="fa fa-bars"></span>
                     </button>
+    
+                    <!-- main menu Search-->
+                    <div class="order-0 col-lg-4 col-md-4 col-sm-12 col-12 mb-3 mb-md-0 ">
+                        <div class="input-group flex-nowrap input-group-lg">
+                            <!-- <button type="button" class="input-group-text" id="addon-wrapping"><i class="fa fa-search"></i></button> -->
+                            <!-- <input type="search" class="form-control" placeholder="Search" aria-label="search" aria-describedby="addon-wrapping"> -->
+                            <!-- <button type="button" class="input-group-text add-member-top" id="addon-wrappingone" data-bs-toggle="modal" data-bs-target="#addUser"><i class="fa fa-plus"></i></button> -->
+                        </div>
+                    </div>
+    
                 </div>
             </nav>
         </div>
